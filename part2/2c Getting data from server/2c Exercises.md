262b6c8ac34dc6dbc3dd27d8725f6ca0decd3ca8
George Pantazes <georgep@wolfram.com>
2019-08-29 11:34:34 -0500

    Finish ex 2.11
    Finish 2.11 fetch data from server (even if just dev server).

---
+++ part2/2c Getting data from server/2c Exercises.md
@@ -0,0 +1,9 @@
+# Part 2c Exercises
+
+# 2.11: The Phonebook Step6
+Continue developing phonebook.
+- [x] Store initial state of the application in a `db.json` in project root
+- [x] Start json-server on port 3001 and make sure that the server returns the list of people by going to the address http://localhost:3001/persons in the browser.
+- [x] Modify the application such that the initial state of the data is fetched from the server using the axios-library.
+    - [x] Complete the fetching with an Effect hook.
+
--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,21 @@
-import React, { useState } from 'react'
-import Person from './components/Person'
+import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
+import axios from 'axios'
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' },
-    { name: 'Gamma Bellas', number: '1234567890' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [nameFilter, setNameFilter] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }, [])
 
   const handleNameFilterChange = event => setNameFilter(event.target.value)
 
